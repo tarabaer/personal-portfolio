4b0f231ddaf6856c1ec610f7171f26cf5654a2ab
Tara Baer <tbaer416@gmail.com>
2021-03-02 19:11:55 -0700

    added star wars

---
+++ code-journal.js
@@ -0,0 +1,37 @@
+/* Variables - containers that store values */
+
+var name // a declared variable but not initialized
+
+let person // declared variable that can be changed
+
+const bar // declared variabl that cannot be changed, not used very much
+
+const ANSWER = 42 // const is declared and initialized with value of 42
+
+// string
+
+let string1 = 'Hello World!' 
+
+let string2 = 'Hello Utah!'
+
+let string3 = new String('Hello World')
+
+// Numbers
+
+let myNum = 2084590348
+
+let myNum = 75.43
+
+'1' == 1 // this statement is true because of type coercion and loose equality checking
+
+'1' === 1 // this statement is false due to STRICT equality checking
+
+// Boolean
+
+let myBool = true
+
+// Array
+
+let myArray = [] // this is an empty array
+
+let myArray = {42, "hoe", ANSWER, true, myBool}
