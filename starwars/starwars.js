73bca262f43ae7b9232e8d9e8a6f1eb4ea3eee6a
Tara Baer <tbaer416@gmail.com>
2021-03-06 16:55:58 -0700

    added film posters

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -6,9 +6,16 @@ let filmlist = document.querySelector('#filmlist')
 
 for (let i = 0; i < films.length; i++) {
     const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
-    let filmItem = document.createElement('li')
-    filmItem.textContent = foundFilm.title
-    filmlist.appendChild(filmItem)
+    let posterFig = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    let figCaption = document.createElement('figcaption')
+
+    figCaption.textContent = foundFilm.title
+    posterFig.appendChild(figImg)
+    posterFig.appendChild(figCaption)
+
+    filmlist.appendChild(posterFig)
 }
 
 function getLastNumber(url) {
