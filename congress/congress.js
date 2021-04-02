4267de228eefd3f824a7e240cdb28aaa805fd66b
Tara Baer <tbaer416@gmail.com>
2021-04-02 16:49:49 -0600

    added buttons and names

--- congress/congress.js
+++ congress/congress.js
@@ -1,3 +1,42 @@
 import { senators } from '../data/senators.js'
 import { representatives } from '../data/representatives.js'
 
+const congressGrid = document.querySelector('.congressGrid')
+const seniorityButton = document.querySelector('#seniorityButton')
+const birthdayButton = document.querySelector('#birthdayButton')
+
+seniorityButton.addEventListener('click', () => {
+    senioritySort()
+})
+
+birthdayButton.addEventListener('click', () => {
+    birthdaySort()
+})
+
+function populateCongressGrid(simplePeople) {
+    simplePeople.forEach(person => {
+        let personDiv = document.createElement('div')
+        let personFig = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        figCaption.textContent = `${person.name}`
+
+        personFig.appendChild(figImg)
+        personFig.appendChild(figCaption)
+        personDiv.appendChild(personFig)
+        congressGrid.appendChild(personDiv)
+    })
+}
+
+function getSimplifiedCongress(congressPeople) {
+    return congressPeople.map(person => {
+        let middleName = person.middle_name ? `${person.middle_name}` : ``
+        return {
+            id: person.id,
+            name: `${person.first_name} ${middleName} ${person.last_name}`
+        }
+    })
+}
+
+populateCongressGrid(getSimplifiedCongress(senators))
