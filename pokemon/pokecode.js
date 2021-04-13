e6b6180f5883ae288fb3b2d3e3daffd5ae4f06f3
Tara Baer <tbaer416@gmail.com>
2021-04-12 19:20:09 -0600

    got choose own pokemon button started

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,8 +1,17 @@
 const pokeGrid = document.querySelector('.pokeGrid')
 const loadButton = document.querySelector('.loadPokemon')
+const chooseButton = document.querySelector('#chooseYourPokemon')
 
 loadButton.addEventListener('click', () => {
     loadPage()
+})
+
+chooseButton.addEventListener('click', () => {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/25`).then(
+        (data) => {
+            populatePokeCard(data)
+        }
+    )
 })
 
 async function getAPIData(url) {
